16e998762204e0be7ea76f760588e130a7f59ff4
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-10-31 21:30:20 -0300

    Create folder DB layer

---
+++ db/folder.js
@@ -0,0 +1,70 @@
+const { PrismaClient } = require("../generated/prisma");
+
+const prisma = new PrismaClient();
+
+async function createFolder(name, parentId = null) {
+  const newFolder = prisma.folder.create({
+    data: {
+      name: name,
+      parentId: parentId,
+    },
+  });
+
+  return newFolder;
+}
+
+async function getFolderContent(folderId) {
+  const folder = await prisma.folder.findUnique({
+    where: {
+      parentId: folderId,
+    },
+    include: {
+      name: true,
+      files: {
+        orderBy: {
+          filename: "asc",
+        },
+      },
+      children: {
+        orderBy: {
+          name: "asc",
+        },
+      },
+    },
+  });
+
+  return { name: folder.name, files: folder.files, children: folder.children };
+}
+
+async function getFolderPath(folderId) {
+  const path = [];
+  let currentId = folderId;
+
+  while (currentId) {
+    const folder = await prisma.folder.findUnique({
+      where: {
+        id: currentId,
+      },
+      select: {
+        id: true,
+        name: true,
+        parentId: true,
+      },
+    });
+
+    if (!folder) {
+      break;
+    }
+
+    path.unshift({ id: folder.id, name: folder.name });
+    currentId = folder.parentId;
+  }
+
+  return path;
+}
+
+module.exports = {
+  createFolder,
+  getFolderContent,
+  getFolderPath,
+};
