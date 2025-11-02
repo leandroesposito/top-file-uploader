e0e734c3992a7c5946553327070c66578c76087a
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-11-02 20:20:39 -0300

    Fix login password check

--- session/Strategy.js
+++ session/Strategy.js
@@ -13,11 +13,12 @@ const Strategy = new LocalStrategy(async (username, password, done) => {
     });
 
     if (!user) {
-      done(null, false, { message: "Incorrect username or password" });
+      return done(null, false, { message: "Incorrect username or password" });
     }
 
-    if (!bcrypt.compare(password, user.password)) {
-      done(null, false, { message: "Incorrect username or password" });
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return done(null, false, { message: "Incorrect username or password" });
     }
 
     done(null, user);
