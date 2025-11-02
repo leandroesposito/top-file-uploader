2dde5dd6d6da39b8a5bcb82f90d9d29b8dbf6dfe
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-11-02 17:04:04 -0300

    Add not authenticated validation and export

--- controllers/auth-validator.js
+++ controllers/auth-validator.js
@@ -1,6 +1,19 @@
 function isAuthenticated(req, res, next) {
   if (!req.isAuthenticated()) {
-    throw new Error("You must log in to continue");
+    throw new Error("You must log in to access this resource!");
   }
   next();
 }
+
+function isNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    req.flash("error", "You must log out to access this resource!");
+    return res.redirect("/");
+  }
+  next();
+}
+
+module.exports = {
+  isAuthenticated,
+  isNotAuthenticated,
+};
